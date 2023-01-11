c89b3f40a01c15c8d33109795bc7a0bbcfc3d3c0
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-11 14:44:53 +0330

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -9,12 +9,16 @@ class MainPortScannerWindow(QWidget):
     def __init__(self):
         super(MainPortScannerWindow, self).__init__()
         self.setWindowTitle("پورت اسکنر")
+        self.top_header_label = QLabel("پورت اسکنر" , self)
+        self.top_header_label.setFont(QFont("irancell" , 20))
+        self.top_header_label.setStyleSheet("color:#0650af;")
+        self.top_header_label.move(425 , 5)
         self.setWindowIcon(QIcon("logo.png"))
         self.setFixedSize(1000 , 500)
         self.app_logo = QLabel(self)
         logo = QPixmap("logo.png")
         self.app_logo.setPixmap(logo)
-        self.app_logo.move(425, 5)
+        self.app_logo.move(425, 50)
 
 window = MainPortScannerWindow()
 window.show()
