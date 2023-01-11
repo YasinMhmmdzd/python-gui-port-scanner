d6ae220ca4b4945e954bc695d3a0440402e0cdb6
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-11 14:25:02 +0330

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -4,8 +4,13 @@ import sys
 from PyQt6.QtGui import QIcon , QFont
 import time
 started_time = time.time()
+app = QApplication(sys.argv)
 class MainPortScannerWindow(QWidget):
     def __init__(self):
         super(MainPortScannerWindow, self).__init__()
         self.setWindowTitle("پورت اسکنر")
         self.setWindowIcon(QIcon("logo.png"))
+
+window = MainPortScannerWindow()
+window.show()
+sys.exit(app.exec())
