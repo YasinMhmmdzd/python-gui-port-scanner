24906097dff1deee3cdcadd8df234ac337f1a3e4
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-11 14:23:25 +0330

    add icon and title

--- port-scanner.py
+++ port-scanner.py
@@ -2,3 +2,10 @@ from socket import *
 from PyQt6.QtWidgets import QApplication , QWidget , QLabel , QPushButton , QLineEdit
 import sys
 from PyQt6.QtGui import QIcon , QFont
+import time
+started_time = time.time()
+class MainPortScannerWindow(QWidget):
+    def __init__(self):
+        super(MainPortScannerWindow, self).__init__()
+        self.setWindowTitle("پورت اسکنر")
+        self.setWindowIcon(QIcon("logo.png"))
